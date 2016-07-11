f831cc5644984981d31e29aebf3846dc5d06a724
Feng George Yu <fyu@ysu.edu>
2016-07-10 23:18:43 -0400

    modified download and list
    the first line of list contains `prefix=url_prefix`, `download.py` will parse the `url_prefix` and use it to download the files with suffixes in the list.txt
    
    **Note** prefix must be quoted or copied directly from your browser

--- download.py
+++ download.py
@@ -1,18 +1,24 @@
 import urllib
 import os
+import time
 
 if not os.path.exists('mp3'):
     os.makedirs('mp3')
 
-fname="sanguo-list.txt"
+fname="list.txt"
 file=open(fname,'r')
+prefix=file.readline()
+prefix=prefix.split("=")[1][:-1]
 for line in file:
-    line=line[:-1]    
-    url='http://yuankuocheng1.zgpingshu.com/%E4%B8%89%E5%9B%BD%E6%BC%94%E4%B9%89/%E4%B8%89%E5%9B%BD%E6%BC%94%E4%B9%89'+urllib.quote(line)+'.mp3'
+    line=line[:-1]
+    if not line:
+        continue        
+    url=prefix+urllib.quote(line)+'.mp3'
     if not os.path.isfile('mp3/'+line+'.mp3'):
-        print('Downloading: '+line)
-        # urllib.urlretrieve(url,'mp3/'+line+'.mp3')
+        print('Downloading: '+line)       
+        #urllib.urlretrieve(url,'mp3/'+line+'.mp3')
         os.system('wget '+url+' -vO mp3/'+line+'.mp3')
+        time.sleep(0.5)
         print('Complete.')
     else:
         print('mp3/'+line+'.mp3 exists')
