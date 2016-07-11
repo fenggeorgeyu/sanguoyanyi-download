83a690acea26a68992723b8f6f990cfdc0a2cef5
Feng George Yu <fyu@ysu.edu>
2016-07-10 22:38:28 -0400

    a

--- download.py
+++ download.py
@@ -11,12 +11,15 @@ for line in file:
     url='http://yuankuocheng1.zgpingshu.com/%E4%B8%89%E5%9B%BD%E6%BC%94%E4%B9%89/%E4%B8%89%E5%9B%BD%E6%BC%94%E4%B9%89'+urllib.quote(line)+'.mp3'
     if not os.path.isfile('mp3/'+line+'.mp3'):
         print('Downloading: '+line)
-        urllib.urlretrieve(url,'mp3/'+line+'.mp3')
+        # urllib.urlretrieve(url,'mp3/'+line+'.mp3')
+        os.system('wget '+url+' -vO mp3/'+line+'.mp3')
         print('Complete.')
     else:
         print('mp3/'+line+'.mp3 exists')
 
 print('All downloading complete')
+
+
 
 
 
